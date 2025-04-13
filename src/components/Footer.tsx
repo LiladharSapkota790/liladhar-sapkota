09e7129ee0ec6e457576d7b9064d736cf9a535b1
Liladhar Sapkota <80949506+LiladharSapkota790@users.noreply.github.com>
2025-04-13 12:02:28 +1000

    footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,7 +10,7 @@ const Footer = () => {
         <div className="flex flex-col md:flex-row justify-between items-center">
           <div className="mb-6 md:mb-0">
             <h2 className="text-2xl font-bold">
-              Liladhar<span className="text-tech-blue">.dev</span>
+              Liladhar<span className="text-tech-blue"> Sapkota</span>
             </h2>
             <p className="text-gray-400 mt-2">IT Support Specialist | Technical Support Specialist</p>
           </div>
